500031845a5e6ecf5c766ed87bb540e985b35107
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-01-16 23:36:15 +0530

    printing the coordinates of the cuboid

---
+++ 3_dcoordinate.py
@@ -0,0 +1,14 @@
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+z = int(input("Enter the value of z: "))
+n = int(input("Enter the value of n: "))
+
+result = []
+
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i +j +k != n:
+                result.append([i,j,k])
+
+print(result)
