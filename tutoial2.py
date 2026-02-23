f433b13e4af153a07c80747a58dd9808f08c8e81
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-02-23 21:29:01 +0530

    list tutorial practice

---
+++ tutoial2.py
@@ -0,0 +1,18 @@
+marks = [30,40,50,60,70]
+# sum_marks = 0
+# size  = len(marks)
+
+# for i in range(0,size):
+#     sum_marks = sum_marks + marks[i]
+
+# print("sum is ", sum_marks)
+
+# print("Averge is ",sum_marks/size)
+
+numbers = [10,20,30,40,50,60,70,80,90,100,110,120,140,160]
+
+print(numbers[7:10])
+print(numbers[-7:-4])
+
+print(*numbers)
+print(numbers)
