6583396643660e93abeac49793299f310dd82107
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-04-17 22:09:16 +0530

    tutorial practice

---
+++ practise_tutoial_session_4.py
@@ -0,0 +1,27 @@
+returned_books = ["Python","DSA","Java","Python","C++"]
+
+unique_books = set(returned_books)
+
+hello = {
+    "a":1,
+    "a":2
+}
+
+# print("delete", hello.pop("name"))
+# print("delete", hello.("name"))
+# print("delete", hello.pop("name"))
+
+print(hello)
+
+print(unique_books[0])
+# print(unique_books)
+
+student = {
+    "name":"Tim",
+    "age" : 12,
+    "course" : "CSE"
+}
+
+print(student.get("name"))
+student["age"] = 15
+print(student)
